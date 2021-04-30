c22628dac9ca701bfdf9e32776181cc70638cb58
Gabriel Ortiz Contreras <>
2021-04-30 13:05:56 +0000

    added Page 3

---
+++ script.js
@@ -0,0 +1,14 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+function myFunction() {
+  document.getElementById("total").innerHTML = "Total in dollar";
+}
+
+
+
